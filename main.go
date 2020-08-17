ffbc46a4ca2585946c083fad60157523f4785f8d
Marcel Lanz <marcellanz@n-1.ch>
2020-08-18 00:36:31 +0200

    [load] load sequences.

--- main.go
+++ main.go
@@ -17,40 +17,84 @@ type msg struct {
 
 func main() {
 	var r cue.Runtime
-	//instance, err := r.Compile("./proto/protocol/cloudstate/discover_seq.cue", nil)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//instance.Value()
-
 	instances := load.Instances([]string{
-		"./proto/protocol/cloudstate/entity.cue",
-		"./proto/protocol/cloudstate/discover_seq.cue"},
-		nil,
+		"./discover_seq.cue",
+	},
+		&load.Config{
+			Dir: "./cue",
+		},
 	)
 	for _, i := range instances {
-		fmt.Printf("%+v\n", i.Complete())
-		fmt.Printf("%+v\n", i.PkgName)
+		//fmt.Printf("complete: %+v\n", i.Complete())
+		fmt.Printf("pkg: %+v\n", i.PkgName)
 
 		build, err := r.Build(i)
 		if err != nil {
 			panic(err)
 		}
-		lookup := build.Value().Lookup("msg001")
-		fmt.Printf("exists: %+v\n", lookup.Exists())
 		fmt.Printf("incomplete: %+v\n", build.Incomplete)
-		v := build.Value()
-		fi, err := v.FieldByName("msg001", false)
-		if err != nil {
-			panic(err)
+		lookup := build.Value().Lookup("all")
+		fmt.Printf("exists: %+v\n", lookup.Exists())
+		if !lookup.Exists() {
+			continue
 		}
-		fmt.Printf("field: %+v\n", fi)
 
-		m := &msg{}
-		err = fi.Value.Decode(m)
+		l := lookup.Eval()
+		fmt.Printf("all.kind: %+v\n", l.Kind())
+		fmt.Printf("all.len: %+v\n", l.Len())
+
+		list, err := l.List()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("msg: %+v\n", m)
+		for list.Next() {
+			value := list.Value()
+			fmt.Printf("list.Value: %+v\n", value)
+			eval := value.Eval()
+			fmt.Printf("list.Value: %+v\n", eval)
+			s, _ := value.Struct()
+			fmt.Printf("list.Value: %+v\n", s)
+			f, err := s.FieldByName("seq", false)
+			if err != nil {
+				panic(err)
+			}
+			seq := f.Value.Eval()
+			fmt.Printf("seq: %+v\n", seq)
+
+			sl, err := seq.List()
+			if err != nil {
+				panic(err)
+			}
+			for sl.Next() {
+				se := sl.Value()
+				req, err := se.FieldByName("req", false)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Printf("req: %+v\n", req.Value.Eval())
+			}
+		}
+
+		//if l.Kind() != cue.ListKind {
+		//	panic("all is not a list")
+		//}
+
+		//_, err := v.List()
+		//if err != nil {
+		//	panic(err)
+		//}
+		//list.Next()
+		//fi, err := v.FieldByName("msg001", false)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//fmt.Printf("field: %+v\n", fi)
+		//
+		//m := &msg{}
+		//err = fi.Value.Decode(m)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//fmt.Printf("msg: %+v\n", m)
 	}
 }
